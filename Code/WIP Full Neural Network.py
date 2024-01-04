a140bfe979c1fb61f2aaa96f0b87a94745c0af20
David Lindahl <s234817@dtu.dk>
2024-01-04 10:43:39 +0100

    Updated read_mp3
    With duration

--- Code/WIP Full Neural Network.py
+++ Code/WIP Full Neural Network.py
@@ -20,8 +20,8 @@ model.classifier[1] = torch.nn.Sequential(
     torch.nn.Sigmoid())
 
 # Function to read MP3 file using librosa
-def read_mp3(filename, as_float=True):
-    sound, sample_rate = librosa.load(filename, sr=None, mono=True)
+def read_mp3(filename, as_float=True, duration = 2.0): # Change duration here
+    sound, sample_rate = librosa.load(filename, sr=None, mono=True, duration= duration)
     if as_float:
         sound = sound.astype(float)
     return sample_rate, sound
