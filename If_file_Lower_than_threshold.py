b8ee9399949f147151c0f0975b3776e491cdd35e
David Lindahl <s234817@dtu.dk>
2024-01-09 10:38:03 +0100

    minichange

--- If_file_Lower_than_threshold.py
+++ If_file_Lower_than_threshold.py
@@ -8,7 +8,7 @@ def read_mp3(filename, as_float=True): # Change duration here
     return sample_rate, sound
 
 
-def is_lower_than_threshold(start_path , threshold:float, destination):
+def is_higher_than_threshold(start_path , threshold:float, destination):
     '''Kode som tager path af mappe, spytter ud en liste med filer der er under en threshold.
     path: The path that you want to move files from
     threshold: seconds. If a file is lower than the threshold, they will me moved
@@ -17,17 +17,23 @@ def is_lower_than_threshold(start_path , threshold:float, destination):
         file_path = os.path.join(start_path,file)
         sample_rate , wav = read_mp3(start_path+file)
         length_in_seconds = len(wav) / sample_rate
-        if length_in_seconds < threshold:
+        if length_in_seconds > threshold:
             destination_file_path_with_file = os.path.join(destination,file)
             
             # Check if the destination path exists, create it if not
             if not os.path.exists(destination):
                 os.makedirs(destination)
 
-            #Making the new file
-            os.rename(file_path, destination)
+            #Making the new file IF less than 100 files
+            if len(os.listdir(destination)) < 100:
+                os.rename(file_path, destination_file_path_with_file)
+            else:
+                return
 
-start_path = '/Users/davidlindahl/Desktop/GitHub/singing_speech_NN-main/audio/test/speech/'
-destination = '/Users/davidlindahl/Desktop/GitHub/singing_speech_NN-main/audio/train/speech/'
-threshold = 5.0
-is_lower_than_threshold(start_path,10.0,destination)
+start_path = '/Users/davidlindahl/Desktop/GitHub/singing_speech_NN-main/audio/train/speech/'
+destination = '/Users/davidlindahl/Desktop/GitHub/singing_speech_NN-main/audio/test/speech/'
+threshold = 7
+is_higher_than_threshold(start_path,7.0,destination)
+
+
+
